3e33db0c78f49a7ebd6627e87f93fadbb4b2d571
robert <bobtomlin@gmail.com>
2021-03-03 11:29:24 -0500

    first commit

--- src/config/fbconfig.js
+++ src/config/fbconfig.js
@@ -3,13 +3,7 @@ import "firebase/firestore";
 
 
 const firebaseConfig = {
-    // apiKey: "AIzaSyDHsWF_mzvWnpmgNJdNYZOPnuDKDtBN4XM",
-    // authDomain: "gravitywellnotes.firebaseapp.com",
-    // databaseURL: "https://gravitywellnotes-default-rtdb.firebaseio.com",
-    // projectId: "gravitywellnotes",
-    // storageBucket: "gravitywellnotes.appspot.com",
-    // messagingSenderId: "864342904837",
-    // appId: "1:864342904837:web:21c45b7f29082628624c6c"
+
   
     apiKey:process.env.REACT_APP_APIKEY,
     authDomain:process.env.REACT_APP_AUTHDOMAINURL,
